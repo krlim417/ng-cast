db7918190c1afaea38d3f6099882acd305741828
krlim417 <krlim417@gmail.com>
2018-04-19 22:51:45 -0700

    Add current video to pass down to videoPlayer component

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ angular.module('video-player')
 
   .controller('AppCtrl', function() {
     this.videos = window.exampleVideoData;
+    this.currentVideo = this.videos[0];
   })
   .component('app', {
     controller: 'AppCtrl',
