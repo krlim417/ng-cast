1ad49ef3705b5893ac050ffa6e3e537dcca297bc
krlim417 <krlim417@gmail.com>
2018-04-19 23:14:05 -0700

    Change current video if user clicks on video title from video entry

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,9 @@ angular.module('video-player')
   .controller('AppCtrl', function() {
     this.videos = window.exampleVideoData;
     this.currentVideo = this.videos[0];
+    this.selectVideo = (clickedVideo) => {
+      this.currentVideo = clickedVideo;
+    };
   })
   .component('app', {
     controller: 'AppCtrl',
