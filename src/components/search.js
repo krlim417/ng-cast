2c26083d36fb272bc10f5a6c06f4ccf3bab472c7
krlim417 <krlim417@gmail.com>
2018-04-26 00:02:18 -0700

    Add bindings and search template, and create function to search for videos

--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,17 @@
 angular.module('video-player')
 
-.component('search', {
-  // TODO
-});
+  .controller('SearchController', function() {
+    this.handleClick = (query) => {
+      this.service.search(this.input, (data) => {
+        this.result(data);
+      });
+    };
+  })
+  .component('search', {
+    bindings: {
+      result: '<',
+      service: '<'
+    },
+    controller: 'SearchController',
+    templateUrl: 'src/templates/search.html' 
+  });
