0cbd1f3666059ce995cdbb591d0cffaaf0d75d63
krlim417 <krlim417@gmail.com>
2018-04-25 23:57:59 -0700

    Inject youTube service, change videos to search results, and load live data

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 angular.module('video-player')
 
-  .controller('AppCtrl', function() {
-    this.videos = window.exampleVideoData;
-    this.currentVideo = this.videos[0];
+  .controller('AppCtrl', function(youTube) {
+    this.searchService = youTube;
+    this.searchResults = (data) => {
+      this.videos = data;
+      this.currentVideo = data[0];
+    };
     this.selectVideo = (clickedVideo) => {
       this.currentVideo = clickedVideo;
     };
+
+    youTube.search('The Duck Song', this.searchResults);
   })
   .component('app', {
     controller: 'AppCtrl',
